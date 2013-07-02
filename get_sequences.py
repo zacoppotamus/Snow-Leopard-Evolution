fe14e4cd4fe37f892ee0e5daaef5f6f6d65dd884
Zac Ioannidis <zacoppotamus@gmail.com>
2013-07-02 15:44:03 -0400

    Fetch cytb and cytc sequences

--- get_sequences.py
+++ get_sequences.py
@@ -4,8 +4,17 @@ import os
 from Bio import SeqIO
 from Bio import Entrez
 
+def save_gbk_file (filename, g_id):
+	if not os.path.isfile(filename):
+	    net_handle = Entrez.efetch(db="nucleotide",id=g_id,rettype="gb", retmode="text")
+	    out_handle = open(filename, "w")
+	    out_handle.write(net_handle.read())
+	    out_handle.close()
+	    net_handle.close()
+	    print "Saved"
+
 Entrez.email = "me@izac.us"
-species = {
+species_mtdna = {
 	"uncia_uncia" : "187250378",
 	"panthera_pardus" : "482514544",
 	"panthera_leo_persica" : "482514544",
@@ -14,18 +23,39 @@ species = {
 	"felis_catus" : "1098523",
 	"herpestes_javanicus" : "57014054"
 }
+species_cytb = {
+	"uncia_uncia" : "402234444",
+	"panthera_pardus" : "187250361",
+	"panthera_leo_persica" : "482514557",
+	"panthera_tigris_sumatrae" : "339716955",
+	"neofelis_nebulosa" : "115531622",
+	"felis_catus" : "68299573",
+	"herpestes_javanicus" : "58578661"
+}
 
-for specie in species:
+species_cytc = {
+	"uncia_uncia" : "145558799",
+	"panthera_pardus" : "187250351",
+	"panthera_leo_persica" : "482514547",
+	"panthera_tigris_sumatrae" : "339716945",
+	"neofelis_nebulosa" : "115531612",
+	"felis_catus" : "5835208",
+	"herpestes_javanicus" : "58578653"
+}
+
+for specie in species_mtdna:
 	filename = specie + ".gbk"
-	if not os.path.isfile(filename):
-	    net_handle = Entrez.efetch(db="nucleotide",id=species[specie],rettype="gb", retmode="text")
-	    out_handle = open(filename, "w")
-	    out_handle.write(net_handle.read())
-	    out_handle.close()
-	    net_handle.close()
-	    print "Saved"
+	save_gbk_file(filename, species_mtdna[specie])
 
-for specie in species:
+for specie in species_cytb:
+	filename = specie + "_cytb.gbk"
+	save_gbk_file(filename, species_cytb[specie])
+
+for specie in species_cytc:
+	filename = specie + "_cytc.gbk"
+	save_gbk_file(filename, species_cytc[specie])
+
+for specie in species_mtdna:
 	filename = specie + ".gbk"
 	for seq_record in SeqIO.parse(filename,"genbank"):
 		print specie
@@ -37,8 +67,6 @@ for specie in species:
 			occurences = sequence.count(base)
 			print base, occurences, "%:", float(occurences)/sequence_length
 		print "\n"
-
-
 
 
 
