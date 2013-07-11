520a3adb42c65c17c482ea56478871d83fbcf922
Zac Ioannidis <zacoppotamus@gmail.com>
2013-07-11 16:49:54 -0400

    Use muscle alignment tool for cytb

--- seqpdistance.py
+++ seqpdistance.py
@@ -6,19 +6,13 @@ from cogent import PROTEIN, DNA
 from cogent.phylo import distance
 from cogent.evolve.models import AH96_mtmammals, JC69
 from cogent.phylo import nj, distance
+from cogent.app.muscle_v38 import align_unaligned_seqs
 import sys
 
 def phyl_tree():
 	mytree = nj.nj(d.getPairwiseDistances())
 	print ("\n\n")
 	print mytree.asciiArt()
-
-al = LoadSeqs("cytb.fasta", moltype=PROTEIN, interleaved=False)
-d = distance.EstimateDistances(al, submodel = AH96_mtmammals())
-d.run()
-sys.stdout = open("cytb distances.txt", "w")
-print d
-phyl_tree()
 
 al = LoadSeqs("cytc.fasta", moltype=PROTEIN, interleaved=False)
 d = distance.EstimateDistances(al, submodel = AH96_mtmammals())
@@ -34,4 +28,10 @@ sys.stdout = open("mtdna distances.txt", "w")
 print d
 phyl_tree()
 
-# Strip sequence for every specie, align it with red fox and get their pairwise distance
+seqs = LoadSeqs("cytb.fasta", moltype=PROTEIN, aligned=False)
+al = align_unaligned_seqs(seqs,PROTEIN)
+d = distance.EstimateDistances(al, submodel = AH96_mtmammals())
+d.run()
+sys.stdout = open("cytb distances.txt", "w")
+print d
+phyl_tree()
