68f6db34302a19415a7dbe9baa6bddc9d28dd024
Zac Ioannidis <zacoppotamus@gmail.com>
2013-07-16 14:20:15 -0400

    Ultimately chose JTT92 submodel

--- seqpdistance.py
+++ seqpdistance.py
@@ -1,10 +1,9 @@
 #!usr/bin/python
-#Calculate pairwise sequence distance
+# Calculate pairwise sequence distance
 
 from cogent import LoadSeqs
 from cogent import PROTEIN, DNA
-from cogent.phylo import distance
-from cogent.evolve.models import AH96_mtmammals, JC69
+from cogent.evolve.models import JTT92, JC69
 from cogent.phylo import nj, distance
 from cogent.app.muscle_v38 import align_unaligned_seqs
 import sys
@@ -15,7 +14,7 @@ def phyl_tree():
 	print mytree.asciiArt()
 
 al = LoadSeqs("cytc.fasta", moltype=PROTEIN, interleaved=False)
-d = distance.EstimateDistances(al, submodel = AH96_mtmammals())
+d = distance.EstimateDistances(al, submodel = JTT92())
 d.run()
 sys.stdout = open("cytc distances.txt", "w")
 print d
@@ -30,8 +29,12 @@ phyl_tree()
 
 seqs = LoadSeqs("cytb.fasta", moltype=PROTEIN, aligned=False)
 al = align_unaligned_seqs(seqs,PROTEIN)
-d = distance.EstimateDistances(al, submodel = AH96_mtmammals())
-d.run()
+dcalc = distance.EstimateDistances(al, submodel = JTT92())
+dcalc.run(show_progress = True)
+d = dcalc.getPairwiseDistances()
+tree=nj.nj(d)
 sys.stdout = open("cytb distances.txt", "w")
-print d
-phyl_tree()
+print dcalc
+print '\n\n'
+print tree.asciiArt()
+#phyl_tree()
